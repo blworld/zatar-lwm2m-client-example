44afe475b1f44d9c0b1b2033da28601e8511cdf1
Josh Larson <jlarson@zebra.com>
2015-06-22 16:20:31 -0500

    Promote "dev" to field in test

--- src/test/java/com/zatar/example/model/DeviceTest.java
+++ src/test/java/com/zatar/example/model/DeviceTest.java
@@ -7,14 +7,20 @@ import org.eclipse.leshan.client.resource.LwM2mInstanceEnabler;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.Value;
 import org.eclipse.leshan.core.response.ValueResponse;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DeviceTest {
 
+	private LwM2mInstanceEnabler dev;
+
+	@Before
+	public void setup() {
+		dev = new Device();
+	}
+
 	@Test
 	public void canReadManufacturer() {
-		final LwM2mInstanceEnabler dev = new Device();
-
 		final ValueResponse response = dev.read(0);
 
 		assertEquals(ResponseCode.CONTENT, response.getCode());
@@ -24,8 +30,6 @@ public class DeviceTest {
 
 	@Test
 	public void canReadModel() {
-		final LwM2mInstanceEnabler dev = new Device();
-
 		final ValueResponse response = dev.read(1);
 
 		assertEquals(ResponseCode.CONTENT, response.getCode());
