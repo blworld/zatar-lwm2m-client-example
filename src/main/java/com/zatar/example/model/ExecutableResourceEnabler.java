515b5fae21aa9404af97d70e3d7acc1981fd08a5
Josh Larson <jlarson@zebra.com>
2015-06-23 12:41:19 -0500

    Introduce new ExecutableResourceEnabler for reboot

---
+++ src/main/java/com/zatar/example/model/ExecutableResourceEnabler.java
@@ -0,0 +1,12 @@
+package com.zatar.example.model;
+
+import org.eclipse.leshan.core.response.ValueResponse;
+
+public class ExecutableResourceEnabler extends BaseResourceEnabler {
+
+	@Override
+	public ValueResponse read() {
+		return null;
+	}
+
+}
