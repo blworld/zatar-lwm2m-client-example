004b75d1b6190150e27436dfe34a225b76c1556d
Josh Larson <jlarson@zebra.com>
2015-06-23 16:33:35 -0500

    Add token resource to DevToken

--- src/main/java/com/zatar/example/model/DevToken.java
+++ src/main/java/com/zatar/example/model/DevToken.java
@@ -16,6 +16,7 @@ public class DevToken extends BaseZatarInstanceEnabler {
 
 	private static Map<Integer, ResourceEnabler> createEnablers() {
 		final HashMap<Integer, ResourceEnabler> resources = new HashMap<>();
+		resources.put(0, new ReadOnlyResourceEnabler(0, "initial-dev-token"));
 		return resources;
 	}
 
