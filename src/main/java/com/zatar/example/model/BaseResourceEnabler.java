260517d182f9d2836a500abf8c8c9f86472ef022
Josh Larson <jlarson@zebra.com>
2015-06-23 12:43:58 -0500

    Promote METHOD_NOT_ALLOWED read to BaseResourceEnabler

--- src/main/java/com/zatar/example/model/BaseResourceEnabler.java
+++ src/main/java/com/zatar/example/model/BaseResourceEnabler.java
@@ -3,8 +3,14 @@ package com.zatar.example.model;
 import org.eclipse.leshan.ResponseCode;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.response.LwM2mResponse;
+import org.eclipse.leshan.core.response.ValueResponse;
 
 public abstract class BaseResourceEnabler implements ResourceEnabler {
+
+	@Override
+	public ValueResponse read() {
+		return new ValueResponse(ResponseCode.METHOD_NOT_ALLOWED);
+	}
 
 	@Override
 	public LwM2mResponse write(final LwM2mResource node) {
--- src/main/java/com/zatar/example/model/ExecutableResourceEnabler.java
+++ src/main/java/com/zatar/example/model/ExecutableResourceEnabler.java
@@ -1,13 +1,5 @@
 package com.zatar.example.model;
 
-import org.eclipse.leshan.ResponseCode;
-import org.eclipse.leshan.core.response.ValueResponse;
-
 public class ExecutableResourceEnabler extends BaseResourceEnabler {
-
-	@Override
-	public ValueResponse read() {
-		return new ValueResponse(ResponseCode.METHOD_NOT_ALLOWED);
-	}
 
 }
