8cae6ab34d05a4948cc69986523d33da0c47d1cc
Josh Larson <jlarson@zebra.com>
2015-06-23 14:07:56 -0500

    Extract population of resource map to helper method

--- src/main/java/com/zatar/example/model/Device.java
+++ src/main/java/com/zatar/example/model/Device.java
@@ -11,14 +11,17 @@ public class Device extends BaseZatarInstanceEnabler {
 	private static final String INITIAL_UTC_OFFSET = "+05";
 
 	public Device() {
+		this.resources = createResourceEnablers();
+	}
+
+	private Map<Integer, ResourceEnabler> createResourceEnablers() {
 		final Map<Integer, ResourceEnabler> resources = new HashMap<>();
 		resources.put(0, new ReadOnlyResourceEnabler(0, MANUFACTURER));
 		resources.put(1, new ReadOnlyResourceEnabler(1, MODEL));
 		resources.put(2, new ReadOnlyResourceEnabler(2, SERIAL_NUMBER));
 		resources.put(4, new ExecutableResourceEnabler());
 		resources.put(14, new ReadWriteResourceEnabler(14, INITIAL_UTC_OFFSET));
-
-		this.resources = resources;
+		return resources;
 	}
 
 }
