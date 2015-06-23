d2cf775289800e706b6e1f6697f93fd86c86256a
Josh Larson <jlarson@zebra.com>
2015-06-23 12:34:17 -0500

    Promote METHOD_NOT_ALLOWED response into BaseResourceEnabler

--- src/main/java/com/zatar/example/model/BaseResourceEnabler.java
+++ src/main/java/com/zatar/example/model/BaseResourceEnabler.java
@@ -1,5 +1,14 @@
 package com.zatar.example.model;
 
+import org.eclipse.leshan.ResponseCode;
+import org.eclipse.leshan.core.node.LwM2mResource;
+import org.eclipse.leshan.core.response.LwM2mResponse;
+
 public abstract class BaseResourceEnabler implements ResourceEnabler {
+
+	@Override
+	public LwM2mResponse write(final LwM2mResource node) {
+		return new LwM2mResponse(ResponseCode.METHOD_NOT_ALLOWED);
+	}
 
 }
--- src/main/java/com/zatar/example/model/ReadOnlyResourceEnabler.java
+++ src/main/java/com/zatar/example/model/ReadOnlyResourceEnabler.java
@@ -3,7 +3,6 @@ package com.zatar.example.model;
 import org.eclipse.leshan.ResponseCode;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.Value;
-import org.eclipse.leshan.core.response.LwM2mResponse;
 import org.eclipse.leshan.core.response.ValueResponse;
 
 public class ReadOnlyResourceEnabler extends BaseResourceEnabler {
@@ -18,11 +17,6 @@ public class ReadOnlyResourceEnabler extends BaseResourceEnabler {
 	@Override
 	public ValueResponse read() {
 		return new ValueResponse(ResponseCode.CONTENT, new LwM2mResource(id, Value.newStringValue(value)));
-	}
-
-	@Override
-	public LwM2mResponse write(final LwM2mResource node) {
-		return new LwM2mResponse(ResponseCode.METHOD_NOT_ALLOWED);
 	}
 
 }
