b09e4f52f54074cd09c78759b8f965a5b3d8aebf
Josh Larson <jlarson@zebra.com>
2015-06-24 16:18:29 -0500

    Extract endpoint and generate it randomly

--- zatar-hello-world/src/main/java/com/zatar/example/main/ExampleLwM2mDeviceMain.java
+++ zatar-hello-world/src/main/java/com/zatar/example/main/ExampleLwM2mDeviceMain.java
@@ -1,7 +1,7 @@
 package com.zatar.example.main;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.UUID;
 
 import org.eclipse.leshan.client.LwM2mClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
@@ -19,29 +19,30 @@ public class ExampleLwM2mDeviceMain {
 		final ObjectsInitializer initializer = new ObjectsInitializer();
 		initializer.setClassForObject(3, Device.class);
 
-		final LwM2mClient client = new LeshanClientBuilder()
-				.setBindingMode(BindingMode.T)
-				.setServerAddress(new InetSocketAddress("lwm2m", 5683))
-				.setObjectsInitializer(initializer)
-				.build(3);
+		final LwM2mClient client = new LeshanClientBuilder().
+				setBindingMode(BindingMode.T).
+				setServerAddress(new InetSocketAddress("lwm2m", 5683)).
+				setObjectsInitializer(initializer).
+				build(3);
 
 		client.start();
-		final RegisterResponse response = client.send(new RegisterRequest("example-endpoint"));
+		final String endpoint = UUID.randomUUID().toString();
+		final RegisterResponse response = client.send(new RegisterRequest(endpoint));
 		final String registrationID = response.getRegistrationID();
 		System.out.println("Registered with ID: " + registrationID);
 
-		waitForInput();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
 
-		client.send(new DeregisterRequest(registrationID));
-		client.stop();
-	}
+			@Override
+			public void run() {
+				if (registrationID != null) {
+					System.out.println("\tDevice: Deregistering Client '" + registrationID + "'");
+					client.send(new DeregisterRequest(registrationID));
+					client.stop();
+				}
+			}
 
-	private static void waitForInput() {
-		try {
-			System.in.read();
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
+		});
 	}
 
 }
