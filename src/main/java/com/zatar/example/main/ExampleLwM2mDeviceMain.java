c753ad205520917b299c255444560b693035a4b9
Josh Larson <jlarson@zebra.com>
2015-06-24 13:07:31 -0500

    Add object model initialization

--- src/main/java/com/zatar/example/main/ExampleLwM2mDeviceMain.java
+++ src/main/java/com/zatar/example/main/ExampleLwM2mDeviceMain.java
@@ -2,28 +2,43 @@ package com.zatar.example.main;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.leshan.client.LwM2mClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
 import org.eclipse.leshan.client.resource.ObjectsInitializer;
+import org.eclipse.leshan.core.model.LwM2mModel;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.model.ResourceModel;
+import org.eclipse.leshan.core.model.ResourceModel.Operations;
+import org.eclipse.leshan.core.model.ResourceModel.Type;
 import org.eclipse.leshan.core.request.BindingMode;
 import org.eclipse.leshan.core.request.DeregisterRequest;
 import org.eclipse.leshan.core.request.RegisterRequest;
 import org.eclipse.leshan.core.response.RegisterResponse;
 
+import com.zatar.example.model.DevToken;
 import com.zatar.example.model.Device;
 
 public class ExampleLwM2mDeviceMain {
 
 	public static void main(final String[] args) {
-		final ObjectsInitializer initializer = new ObjectsInitializer();
+		final Map<Integer, ObjectModel> objectModels = new HashMap<>();
+		objectModels.put(3, createDeviceObjectModel());
+		objectModels.put(23854, createDevTokenObjectModel());
+
+		final LwM2mModel model = new LwM2mModel(objectModels);
+		final ObjectsInitializer initializer = new ObjectsInitializer(model);
+
 		initializer.setClassForObject(3, Device.class);
+		initializer.setClassForObject(23854, DevToken.class);
 
 		final LwM2mClient client = new LeshanClientBuilder()
 				.setBindingMode(BindingMode.T)
 				.setServerAddress(new InetSocketAddress("lwm2m", 5683))
 				.setObjectsInitializer(initializer)
-				.build(3);
+				.build(3, 23854);
 
 		client.start();
 		final RegisterResponse response = client.send(new RegisterRequest("example-endpoint"));
@@ -34,6 +49,23 @@ public class ExampleLwM2mDeviceMain {
 
 		client.send(new DeregisterRequest(registrationID));
 		client.stop();
+	}
+
+	private static ObjectModel createDeviceObjectModel() {
+		final Map<Integer, ResourceModel> resources = new HashMap<Integer, ResourceModel>();
+		resources.put(0, new ResourceModel(0, "Manufacturer", Operations.R, false, false, Type.STRING, "", "", ""));
+		resources.put(1, new ResourceModel(1, "Model", Operations.R, false, false, Type.STRING, "", "", ""));
+		resources.put(2, new ResourceModel(2, "Serial Number", Operations.R, false, false, Type.STRING, "", "", ""));
+		resources.put(4, new ResourceModel(4, "Reboot", Operations.E, false, false, Type.STRING, "", "", ""));
+		resources.put(14, new ResourceModel(14, "UTC Offset", Operations.RW, false, false, Type.STRING, "", "", ""));
+		return new ObjectModel(3, "Device", "", false, true, resources);
+	}
+
+	private static ObjectModel createDevTokenObjectModel() {
+		final Map<Integer, ResourceModel> resources = new HashMap<Integer, ResourceModel>();
+		resources.put(0, new ResourceModel(0, "Token", Operations.R, false, false, Type.STRING, "", "", ""));
+		resources.put(3, new ResourceModel(1, "Validation", Operations.W, false, false, Type.INTEGER, "", "", ""));
+		return new ObjectModel(23854, "Zatar Device Token", "", false, true, resources);
 	}
 
 	private static void waitForInput() {
