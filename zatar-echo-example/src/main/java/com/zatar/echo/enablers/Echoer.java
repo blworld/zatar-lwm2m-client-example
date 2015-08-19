277cca669e3cc8ef8cc5fda854fe784c2bc4b5a7
Josh Larson <jlarson@zebra.com>
2015-08-19 13:16:20 -0500

    Can update echo count variable

--- zatar-echo-example/src/main/java/com/zatar/echo/enablers/Echoer.java
+++ zatar-echo-example/src/main/java/com/zatar/echo/enablers/Echoer.java
@@ -4,6 +4,7 @@ import org.eclipse.leshan.ResponseCode;
 import org.eclipse.leshan.client.resource.SimpleInstanceEnabler;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.Value;
+import org.eclipse.leshan.core.response.LwM2mResponse;
 import org.eclipse.leshan.core.response.ValueResponse;
 
 public class Echoer extends SimpleInstanceEnabler {
@@ -16,6 +17,19 @@ public class Echoer extends SimpleInstanceEnabler {
 			return new ValueResponse(ResponseCode.CONTENT, new LwM2mResource(1, Value.newIntegerValue(echoCount)));
 		}
 		return super.read(resourceId);
+	}
+
+	@Override
+	public LwM2mResponse write(final int resourceId, final LwM2mResource res) {
+		if (resourceId == 1) {
+			@SuppressWarnings("unchecked")
+			final Value<Integer> value = (Value<Integer>) res.getValue();
+			echoCount = value.value;
+			System.out.println("Echo Count set to " + echoCount);
+			fireResourceChange(resourceId);
+			return new LwM2mResponse(ResponseCode.CHANGED);
+		}
+		return super.write(resourceId, res);
 	}
 
 }
